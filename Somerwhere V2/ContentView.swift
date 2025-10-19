4dcf5ad42f3645ce0c2b8589e4bfc6332c9da883
Travis Swanson <travis.m.swanson@gmail.com>
2025-10-19 11:31:47 -0700

    Initial Commit

---
+++ Somerwhere V2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Somerwhere V2
+//
+//  Created by Travis Swanson on 10/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Somerwhere V2/Somerwhere_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Somerwhere_V2App.swift
+//  Somerwhere V2
+//
+//  Created by Travis Swanson on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Somerwhere_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

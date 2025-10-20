a8a4ca012e048dfd9d072905cc54f4ee6a05babe
Travis Swanson <travis.m.swanson@gmail.com>
2025-10-19 18:28:58 -0700

    test: add ts-jest config + tsconfig.test.json; ensure jest/node types

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  transformIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.test.json'
+    }
+  },
+  clearMocks: true,
+  transformIgnorePatterns: ['/node_modules/']
 };
